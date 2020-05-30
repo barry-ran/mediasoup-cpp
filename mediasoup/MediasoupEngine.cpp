129ae38bcbe3d7599d8162ad7f9a0bb271dbcae0
rankun <rankun@bytedance.com>
2020-05-30 17:33:04 +0800

    feat: add promise
    Change-Id: I263f5367f4b4e5bd0fad7c0d6ac18b4d93db5485

--- mediasoup/MediasoupEngine.cpp
+++ mediasoup/MediasoupEngine.cpp
@@ -1,6 +1,9 @@
 #include "MediasoupEngine.hpp"
 #include "Logger.hpp"
 #include "EventEmitter.hpp"
+#include "Promise.hpp"
+
+using namespace promise;
 
 namespace mediasoup
 {
@@ -26,6 +29,40 @@ bool MediasoupEngine::Init() {
 
     MS_lOGGERD("emit evnet 0");
     emitter.emit(0, "from emit", 777);
+
+    // test Promise
+    newPromise([](Defer d){
+        MS_lOGGERD("promise http request 1");
+        int ret = 0;
+        //ret = 1;
+        if (0 == ret) {
+            d.resolve();
+        } else {
+            d.reject(ret);
+        }
+    }).then([](){ // on resolve
+        MS_lOGGERD("http request 1 success");
+        Defer next = newPromise([](Defer d) {
+            MS_lOGGERD("promise http request 2");
+            int ret = 0;
+            ret = 1;
+            if (0 == ret) {
+                d.resolve();
+            } else {
+                d.reject(ret);
+            }
+        });
+        //Will call next.resole() or next.reject() later
+        return next;
+    }, [](int ret){ // on reject
+        MS_lOGGERE("http request 1 failed:{}", ret);
+    }).then([](){ // on resolve
+        MS_lOGGERE("http request 2 success");
+    }, [](int ret){ // on reject
+        MS_lOGGERE("http request 2 failed:{}", ret);
+    }).always([](){
+        MS_lOGGERD("http request finish");
+    });
 
 	return true;
 }
