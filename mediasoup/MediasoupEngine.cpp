9beb7a5df24a57a7c9651bcfc9ae05f4024bcef1
rankun <rankun@bytedance.com>
2020-05-30 16:07:09 +0800

    feat: add EventEmitter
    Change-Id: I61130d5ed0982e480eb76290c27a1cfef29182ad

--- mediasoup/MediasoupEngine.cpp
+++ mediasoup/MediasoupEngine.cpp
@@ -1,5 +1,6 @@
 #include "MediasoupEngine.hpp"
 #include "Logger.hpp"
+#include "EventEmitter.hpp"
 
 namespace mediasoup
 {
@@ -16,6 +17,16 @@ bool MediasoupEngine::Init() {
     MS_lOGGERD("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
     MS_lOGGERW("Some error message with arg: {}", 1);
     MS_lOGGERE("Easy padding in numbers like {:08d}", 12);
+
+    // test EventEmitter
+    EventEmitter emitter;
+    emitter.on(0, [](const char *text, int n) { 
+        MS_lOGGERD("recv emitter evnet 0:{} {}", text, n);
+    });
+
+    MS_lOGGERD("emit evnet 0");
+    emitter.emit(0, "from emit", 777);
+
 	return true;
 }
 
