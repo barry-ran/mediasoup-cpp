59bd5cbfcb9b3decbcb9e204ca8c1eddd4ab7c6d
rankun <rankun@bytedance.com>
2020-07-20 12:50:06 +0800

    feat: remove EventEmitter
    Change-Id: I3e39941f5ee4a4a53d2414c90b971931b2ff40f3

--- mediasoup/Mediasoup.cpp
+++ mediasoup/Mediasoup.cpp
@@ -1,6 +1,5 @@
 #include "Mediasoup.hpp"
 #include "Logger.hpp"
-#include "EventEmitter.hpp"
 #include "Promise.hpp"
 #include "Worker.hpp"
 #include "SupportedRtpCapabilities.hpp"
@@ -34,16 +33,7 @@ void Mediasoup::Test() {
     return;
     MS_lOGGERD("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
     MS_lOGGERW("Some error message with arg: {}", 1);
-    MS_lOGGERE("Easy padding in numbers like {:08d}", 12);
-
-    // test EventEmitter
-    EventEmitter emitter;
-    emitter.on(0, [](const char *text, int n) { 
-        MS_lOGGERD("recv emitter evnet 0:{} {}", text, n);
-    });
-
-    MS_lOGGERD("emit evnet 0");
-    emitter.emit(0, "from emit", 777);
+    MS_lOGGERE("Easy padding in numbers like {:08d}", 12);    
 
     // test Promise
     newPromise([](Defer d){
